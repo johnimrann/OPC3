59ab190553cc0cda648fe9336c30fc21b2d04e16
Imrann John <93659960+johnimrann@users.noreply.github.com>
2024-12-17 20:38:55 +0100

    init: Configuration initiale du projet Salesforce DX
    - Création de la structure du projet
    - Configuration des métadonnées Salesforce
    - Initialisation du package.xml

---
+++ .sfdx/typings/lwc/apex/AccountCasesController.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/AccountCasesController.findCasesBySubject" {
+  export default function findCasesBySubject(param: {accountId: any, subjectSearchTerm: any}): Promise<any>;
+}
